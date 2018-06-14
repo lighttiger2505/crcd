b47127a8429410edfb2ec6898de578ad64c06e5f
toshikazu.ohashi <toshikazu.ohashi@iridge.jp>
2018-06-14 21:24:39 +0900

    Add implements bookmark

--- bookmark.go
+++ bookmark.go
@@ -1,12 +1,93 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"runtime"
 
+	"github.com/fatih/color"
+	homedir "github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli"
 )
 
+type BookmarkRoot struct {
+	Checksum string `json:"checksum"`
+	Version  int    `json:"version"`
+	Roots    struct {
+		SyncTransactionVersion string           `json:"sync_transaction_version"`
+		BookmarkBar            BookmarkChildren `json:"bookmark_bar"`
+		Other                  BookmarkChildren `json:"other"`
+		Synced                 BookmarkChildren `json:"synced"`
+	} `json:"roots"`
+}
+
+type BookmarkChildren struct {
+	ID                     string `json:"id"`
+	Name                   string `json:"name"`
+	Type                   string `json:"type"`
+	URL                    string `json:"url,omitempty"`
+	DateAdded              string `json:"date_added"`
+	DateModified           string `json:"date_modified,omitempty"`
+	SyncTransactionVersion string `json:"sync_transaction_version"`
+	MetaInfo               struct {
+		LastVisitedDesktop string `json:"last_visited_desktop"`
+	} `json:"meta_info,omitempty"`
+	Children []BookmarkChildren `json:"children,omitempty"`
+}
+
 func bookmark(c *cli.Context) error {
-	fmt.Println("bookmark")
+	dbPath := getBookmarkPath(runtime.GOOS)
+
+	bytes, err := ioutil.ReadFile(dbPath)
+	if err != nil {
+		return err
+	}
+
+	var root BookmarkRoot
+	if err := json.Unmarshal(bytes, &root); err != nil {
+		return err
+	}
+
+	bs := collectBookmark(root.Roots.BookmarkBar.Children)
+
+	clr := color.New(color.FgCyan)
+	for _, b := range bs {
+		clr.Print(b.Name)
+		clr.Print("    ")
+		fmt.Println(b.URL)
+	}
+
 	return nil
+}
+
+func getBookmarkPath(goos string) string {
+	home, err := homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+
+	var browserBookmarkPath string
+	switch goos {
+	case "darwin":
+		browserBookmarkPath = "Library/Application Support/Google/Chrome/Default/Bookmarks"
+	case "windows":
+		// browserBookmarkPath = ".config/google-chrome/Default/History"
+	default:
+		browserBookmarkPath = ".config/google-chrome/Default/Bookmarks"
+	}
+
+	return filepath.Join(home, browserBookmarkPath)
+}
+
+func collectBookmark(children []BookmarkChildren) []BookmarkChildren {
+	var results []BookmarkChildren
+	for _, child := range children {
+		if child.Type == "url" {
+			results = append(results, child)
+		}
+		results = append(results, collectBookmark(child.Children)...)
+	}
+	return results
 }
